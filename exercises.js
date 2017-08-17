526c0abd39a1fffb348c0c9c2d7299956fe13d55
Donnie Reese <donniereese@gmail.com>
2017-08-17 12:57:48 -0500

    Finished homework (homework.md) along with Feynman written, passed tests of exercises and uploaded to github.

--- exercises.js
+++ exercises.js
@@ -1,127 +1,214 @@
 // Do not change any of the function names
 
 function getBiggest(x, y) {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+    // x and y are integers.  Return the larger integer
+    // if they are the same return either one
+    /**
+     * Code Notes
+     * conditional Ternary conditional return
+     * short-hand of if-else with return conditions
+     */
+    return (x >= y) ? x : y;
 }
 
 function greeting(language) {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'English' -> 'Hello!'
-  // language: 'Spanish' -> 'Hola!'
-  // if language is undefined return 'Hello!'
+    // return a greeting for three different languages:
+    // language: 'German' -> 'Guten Tag!'
+    // language: 'English' -> 'Hello!'
+    // language: 'Spanish' -> 'Hola!'
+    // if language is undefined return 'Hello!'
+    var greeting;
+    switch(language) {
+        case 'German':
+            greeting = 'Guten Tag!';
+        break;
+        case 'Spanish':
+            greeting = 'Hola!';
+        break;
+        case 'English':
+        default:
+            greeting = 'Hello!';
+    }
+
+    return greeting;
 }
 
 function isTenOrFive(num) {
-  // return true if num is 10 or 5
-  // otherwise return false
+    // return true if num is 10 or 5
+    // otherwise return false
+    /**
+     * Code Notes
+     * Long-hand if-else use
+     * either one or other condition is met
+     * or return false otherwise
+     */
+    if( num === 10 || num === 5 ) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 function isInRange(num) {
-  // return true if num is less than 50 and greater than 20
+    // return true if num is less than 50 and greater than 20
+    return ( num < 50 && num > 20 ) ? true : false;
 }
 
 function isInteger(num) {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
+    // return true if num is an integer
+    // 0.8 -> false
+    // 1 -> true
+    // -10 -> true
+    // otherwise return false
+    // hint: you can solve this using Math.floor
+    return num === Math.floor(num);
 }
 
 function fizzBuzz(num) {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
+    // if num is divisible by 3 return 'fizz'
+    // if num is divisible by 5 return 'buzz'
+    // if num is divisible by 3 & 5 return 'fizzbuzz'
+    // otherwise return num
+    return ( num % 3 === 0 && num % 5 === 0 ) ? 'fizzbuzz' : ( num % 3 === 0 ) ? 'fizz' : ( num % 5 === 0 ) ? 'buzz' : num;
 }
 
 function isPrime(num) {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
+    // return true if num is prime.
+    // otherwise return false
+    // hint: a prime number is only evenly divisible by itself and 1
+    // hint2: you can solve this using a for loop
+    // note: 0 and 1 are NOT considered prime numbers
+    for(var i = 0; i <= num; i++) {
+        if( num < 2 ) {
+            return false;
+        } else if(i >= 2) {
+            if( (num % i === 0 || num  % i === num) && i != num )
+                return false;
+        }
+    }
+    return num > 1;
 }
 
 function returnFirst(arr) {
-  // return the first item from the array
+    // return the first item from the array
+    return arr[0];
 }
 
 function returnLast(arr) {
-  // return the last item of the array
+    // return the last item of the array
+    return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
-  // return the length of the array
+    // return the length of the array
+    return arr.length;
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
-  // increase each integer by one
-  // return the array
+    // arr is an array of integers
+    // increase each integer by one
+    // return the array
+
+    var newArr = arr.map(function(num){
+        return num + 1;
+    });
+
+    return newArr;
 }
 
 function addItemToArray(arr, item) {
-  // add the item to the end of the array
-  // return the array
+    // add the item to the end of the array
+    // return the array
+    arr.push(item);
+    return arr;
 }
 
 function addItemToFront(arr, item) {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
+    // add the item to the front of the array
+    // return the array
+    // hint: use the array method .unshift
+    arr.unshift(item);
+    return arr;
 }
 
 function wordsToSentence(words) {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+    // words is an array of strings
+    // return a string that is all of the words concatenated together
+    // spaces need to be between each word
+    // example: ['Hello', 'world!'] -> 'Hello world!'
+    return words.join(' ');
 }
 
 function contains(arr, item) {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+    // check to see if item is inside of arr
+    // return true if it is, otherwise return false
+
+    // Using Array method...
+    return arr.includes(item);
+
+    /* or
+    *   for( var i = 0; i < arr.length - 1; i++ ) {
+    *       if(arr[i] === item)
+    *           return true;
+    *   }
+    *
+    *   return false;
+    */
 }
 
 function addNumbers(numbers) {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
+    // numbers is an array of integers.
+    // add all of the integers and return the value
+    return numbers.reduce(function(a, b) {
+        return a + b;
+    }, 0);
 }
 
 function averageTestScore(testScores) {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
+    // testScores is an array.  Iterate over testScores and compute the average.
+    // return the average
+    return testScores.reduce(function(sum, next, index, arr){
+        sum = ( arr.length - 1 === index ) ? (sum + next) / arr.length : sum + next;
+        return sum;
+    }, 0);
 }
 
 function largestNumber(numbers) {
-  // numbers is an array of integers
-  // return the largest integer
+    // numbers is an array of integers
+    // return the largest integer
+    var largest = 0;
+    for(var i = 0; i < numbers.length - 1; i++) {
+        largest = ( numbers[i] >= largest ) ? numbers[i] : largest;
+    }
+    return largest;
+
+    // or using reduce...
+    // return numbers.reduce(function(val, next){
+    //     return ( next >= val ) ? next : val;
+    // }, 0);
 }
 
 // Do not modify code below this line.
 // --------------------------------
 
 module.exports = {
-  getBiggest: getBiggest,
-  greeting: greeting,
-  isTenOrFive: isTenOrFive,
-  isInRange: isInRange,
-  isInteger: isInteger,
-  fizzBuzz: fizzBuzz,
-  isPrime: isPrime,
-  returnFirst: returnFirst,
-  returnLast: returnLast,
-  getArrayLength: getArrayLength,
-  incrementByOne: incrementByOne,
-  addItemToArray: addItemToArray,
-  addItemToFront: addItemToFront,
-  wordsToSentence: wordsToSentence,
-  contains: contains,
-  addNumbers: addNumbers,
-  averageTestScore: averageTestScore,
-  largestNumber: largestNumber
+    getBiggest: getBiggest,
+    greeting: greeting,
+    isTenOrFive: isTenOrFive,
+    isInRange: isInRange,
+    isInteger: isInteger,
+    fizzBuzz: fizzBuzz,
+    isPrime: isPrime,
+    returnFirst: returnFirst,
+    returnLast: returnLast,
+    getArrayLength: getArrayLength,
+    incrementByOne: incrementByOne,
+    addItemToArray: addItemToArray,
+    addItemToFront: addItemToFront,
+    wordsToSentence: wordsToSentence,
+    contains: contains,
+    addNumbers: addNumbers,
+    averageTestScore: averageTestScore,
+    largestNumber: largestNumber
 };
---
+++ homework.md
@@ -0,0 +1,60 @@
+# Lesson 1 Homework
+
+## 1. Feynman Writing Promps
+
+* for loop:
+    the `for` command is a complicated-looking command that does something very well. It is simply a loop 
+    that follows a set of instructions until the instructions tell it to stop or until the loop runs to 
+    the end of the guidelines you set it up with. You write out your guidelines on how many times you 
+    want to follow the instructions or when it should end in the `for` loop command.  The instructions in 
+    the brackets are like a recipe.  Most `for` loops are meant to deal with a bunch of things that 
+    are similiar and you can use the same instructions for each thing.  
+    
+    **Example:**
+    "You're mother hands you a box of things and tells you to go through the whole box of toys and look at 
+    each one.  Don't stop until you get through the whole box and only deal with one toy at a time. You 
+    can keep the toy if it's not broken, dirty and if you still want it.  You can get rid of it if you think 
+    you are too big for it.  Tell me if you have any questions."
+    
+    Giving you the box and the part about "go through the whole box, one by one and don't stop until you get 
+    through everything" is the instructions in the `for` loop.  The reasons you can and can't keep the individual
+    toy is the instructions in the foor loop.
+* &&, ||, !:
+    These three things are just simple little shortcuts, like a secret code, that the computer understands.  A 
+    lot of time you would spend writing out code is telling the computer "This and this, but not this".  Someone 
+    who made code wrote these out as shortcuts for AND, OR, and NOT.  You can use `&&` for "and", `||` for 
+    "or" and `!` for "not".  The first two were doubled, just to be specific and obvious for bot the computer 
+    and the programmer.  For example: "!cookies" means no cookies!  I know, terrible!
+* Arrays:
+    Arrays are just lists.  It's a specific list that the computer can look at and say "Oh yea, that there is 
+    a list, alright."  Also, when you use what a computer knows as an `Array`, you are specifically telling 
+    the computer that that is what it is and the computer gives you all sorts of tools to use with it to 
+    keep it in working order.  Each list has it's own set, too!
+* Git:
+    Git is an important thing in computers that most people don't even see.  It's a way for people to share 
+    what they are working on, like their homework or a book, and each person can have their very own copy 
+    and make changes to it.  If they choose to give what they have to everyone else, they use Git to take 
+    what they've done and put it in a main place that everyone else sees.  It keeps all the changes that 
+    everyone makes and makes sure it's all sorted properly.  You can even go back and look at everyone's 
+    changes and undo things you've done.  It's like making new copies everytime, but being able to go back 
+    or make the changes someone else made yours.  Being able to share is great!.
+* Github:
+    Github is a place on the internet, a website, that let's people have their stuff, their Git files in a 
+    secure place away from their computer.  If something happened to their computer, their stuff would stil 
+    be there and they could just use Git to grab their stuff anywhere else they are and keep working on it, 
+    even from a new computer.  They can also use the website to see their stuff and other people's stuff and 
+    even make copies and share their changes with other people.  It's like a bank, but for sharing!  Like 
+    a bank mixed with a museum.
+
+## 2. Install git.
+    **Done!**
+
+## 3. Fork and clone this repo. When you need to commit use the following commands.
+    * `git status`
+    * `git add --all`
+    * `git status`
+    * `git commit -m "your commit message"`
+    * `git push origin master`
+
+## 4. Make the tests pass!
+    **Done!**
